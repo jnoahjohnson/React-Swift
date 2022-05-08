72395d96fd6ffb275aa9266c0a3fdbf3835b46ba
Noah Johnson <jnoahjohnson@gmail.com>
2022-05-07 18:13:50 -0600

    Initial Commit

---
+++ React-Swift/React_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  React_SwiftApp.swift
+//  React-Swift
+//
+//  Created by Noah Johnson on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct React_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
